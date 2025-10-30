1b58509cd4f9b96877889448300fc3b592ba53ce
benjay10 <ben.behaeghe@redpencil.io>
2025-10-30 01:33:15 +0100

    Basic error handler with logging
    Without error handler, the service would crash and not restart after a
    critical error. Now, it logs the error and keeps going for further
    requests.

--- app.js
+++ app.js
@@ -12,49 +12,69 @@ app.use(
   }),
 );
 
-app.get('/bookmarks', async function(req, res) {
-  const sessionUri = getSessionUri(req);
-  const bookmarks = await listBookmarks(sessionUri);
-  const data = bookmarks.map(bookmarkToJsonApi);
-  res.send({ data });
-});
-
-app.post('/public-services/:id/bookmarks', async function(req, res) {
-  const sessionUri = getSessionUri(req);
-  const ipdcService = await getIPDCService(req.params.id);
-  if (ipdcService) {
-    const bookmark = await createBookmark(sessionUri, ipdcService);
-    const data = bookmarkToJsonApi(bookmark);
-    res.status(201).send({ data })
-  } else {
-    res.status(404).send();
+app.get('/bookmarks', async function(req, res, next) {
+  try {
+    const sessionUri = getSessionUri(req);
+    const bookmarks = await listBookmarks(sessionUri);
+    const data = bookmarks.map(bookmarkToJsonApi);
+    res.send({ data });
+  } catch (error) {
+    console.error(error.message);
+    next(error);
   }
 });
 
-app.delete('/bookmarks/:id', async function(req, res) {
-  const sessionUri = getSessionUri(req);
-  const bookmark = await getBookmark(req.params.id, sessionUri);
-  if (bookmark) {
-    await deleteBookmark(bookmark.uri);
-  }
-  res.status(204).send();
-});
-
-app.post('/delta', async function (req, res) {
-  const sessions = new Set();
-  req.body.forEach((changeset) => {
-    changeset.inserts.forEach((insert) => {
-      if (insert.predicate.value === 'http://mu.semte.ch/vocabularies/ext/sessionRole') {
-        sessions.add(insert.subject.value);
-      }
-    });
-  });
-  for (const session of sessions) {
-    const hasPrepopulated = await hasAccountPrepopulatedBookmarks(session);
-    if (!hasPrepopulated) {
-      await prepopulateBookmarksForSession(session);
-      await setAccountPrepopulatedBookmarks(session);
+app.post('/public-services/:id/bookmarks', async function(req, res, next) {
+  try {
+    const sessionUri = getSessionUri(req);
+    const ipdcService = await getIPDCService(req.params.id);
+    if (ipdcService) {
+      const bookmark = await createBookmark(sessionUri, ipdcService);
+      const data = bookmarkToJsonApi(bookmark);
+      res.status(201).send({ data })
+    } else {
+      res.status(404).send();
     }
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
+});
+
+app.delete('/bookmarks/:id', async function(req, res, next) {
+  try {
+    const sessionUri = getSessionUri(req);
+    const bookmark = await getBookmark(req.params.id, sessionUri);
+    if (bookmark) {
+      await deleteBookmark(bookmark.uri);
+    }
+    res.status(204).send();
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
+});
+
+app.post('/delta', async function (req, res, next) {
+  try {
+    const sessions = new Set();
+    req.body.forEach((changeset) => {
+      changeset.inserts.forEach((insert) => {
+        if (insert.predicate.value === 'http://mu.semte.ch/vocabularies/ext/sessionRole') {
+          sessions.add(insert.subject.value);
+        }
+      });
+    });
+    for (const session of sessions) {
+      const hasPrepopulated = await hasAccountPrepopulatedBookmarks(session);
+      if (!hasPrepopulated) {
+        await prepopulateBookmarksForSession(session);
+        await setAccountPrepopulatedBookmarks(session);
+      }
+    }
+  } catch (error) {
+    console.error(error.message);
+    next(error);
   }
 });
 
